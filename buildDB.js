ce0e0175d32c01c5466bf08cbbfb0e493c226da5
gaelder <gaelder@uwaterloo.ca>
2017-09-22 08:57:33 -0400

    This is why you dont accidentally delete scripts... It finally works again

--- buildDB.js
+++ buildDB.js
@@ -1,29 +1,37 @@
 r = require('rethinkdb');
 
-r.dbCreate('layouts');
-
 var connection = null;
+
 r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
     if (err) throw err;
+    console.log('hello' + conn);
     connection = conn;
+    createTables();
 })
 
-r.db('layouts').tableCreate('uris').run(connection, function(err, result) {
-    if (err) throw err;
-    console.log('Created uri table.');
-})
+function createTables(){
+    r.dbCreate('layouts').run(connection, function(err,callback){
+        if (err) throw err;
+        console.log('Database created');
+    });
 
-r.db('layouts').tableCreate('submissions').run(connection, function(err, result) {
-    if (err) throw err;
-    console.log('Created submissions table.');
-})
+    r.db('layouts').tableCreate('uris').run(connection, function(err, result) {
+        if (err) throw err;
+        console.log('Created uri table.');
+    })
 
-r.db('layouts').tableCreate('users').run(connection, function(err, result) {
-    if (err) throw err;
-    console.log('Created users table.');
-})
+    r.db('layouts').tableCreate('submissions').run(connection, function(err, result) {
+        if (err) throw err;
+        console.log('Created submissions table.');
+    })
 
-r.db('layouts').tableCreate('cachedLayouts').run(connection, function(err, result) {
-    if (err) throw err;
-    console.log('Created cached layouts table.');
-})
+    r.db('layouts').tableCreate('users').run(connection, function(err, result) {
+        if (err) throw err;
+        console.log('Created users table.');
+    })
+
+    r.db('layouts').tableCreate('cachedLayouts').run(connection, function(err, result) {
+        if (err) throw err;
+        console.log('Created cached layouts table.');
+    })
+}
