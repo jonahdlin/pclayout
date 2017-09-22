068a40d639d8456312bb292e83ba4e2afd5a8bb0
gaelder <gaelder@uwaterloo.ca>
2017-09-22 08:40:11 -0400

    added other half of file. Oops

--- buildDB.js
+++ buildDB.js
@@ -1,3 +1,13 @@
+r = require('rethinkdb');
+
+r.dbCreate('layouts');
+
+var connection = null;
+r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+    if (err) throw err;
+    connection = conn;
+})
+
 r.db('layouts').tableCreate('uris').run(connection, function(err, result) {
     if (err) throw err;
     console.log('Created uri table.');
